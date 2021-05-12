b3d15afb24297719fb618ecbc047011843e8cbb2
Amanda Tell <amanda@tengai-unbiased.com>
2021-05-12 22:48:20 +0200

    popper installed and started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import { useState, useEffect } from 'react'
 import './App.css';
 import axios from 'axios'
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, Move, Sticky } from "react-scroll-motion";
+import { FiHelpCircle } from 'react-icons/fi';
+import { usePopperTooltip } from 'react-popper-tooltip';
+import 'react-popper-tooltip/dist/styles.css';
 
+/*
+
+IMPORT BUTTON FROM REACT BOOSTRAP
+PUT IN AN COMPONENT
+
+*/
 function App() {
+  const {
+    getArrowProps,
+    getTooltipProps,
+    setTooltipRef,
+    setTriggerRef,
+    visible,
+  } = usePopperTooltip();
+
   const [joke, setJoke] = useState([])
   const [top, setTop] = useState(false)
   const [bottom, setBottom] = useState(false)
@@ -42,7 +58,7 @@ function App() {
     }
     showLine()
 
-  }, [top, bottom, start])
+  }, [top, bottom, start, joke.punchline])
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, {
@@ -57,8 +73,23 @@ function App() {
   const FadeUp = batch(Fade(), Move(), Sticky());
   return (
     <div className="App container">
+
+
       <ScrollContainer>
         <ScrollPage page={0}>
+          <h1>Scroll a joke! <button type="button" ref={setTriggerRef}>
+            <FiHelpCircle />
+          </button>
+            {visible && (
+              <div
+                ref={setTooltipRef}
+                {...getTooltipProps({ className: 'tooltip-container' })}
+              >
+                <div {...getArrowProps({ className: 'tooltip-arrow' })} />
+          Tooltip
+              </div>
+            )}
+          </h1>
           <Animator animation={FadeUp}>
             <span id="setup" style={{ fontSize: "40px" }}></span>
           </Animator>
