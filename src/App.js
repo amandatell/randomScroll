8f8d6f38c62e7e2ec32898b085ccb94741df14c2
Amanda Tell <amanda@tengai-unbiased.com>
2021-05-12 16:06:22 +0200

    create app with scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 
 function App() {
+
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Move(), Sticky());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App container">
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={FadeUp}>
+            <span style={{ fontSize: "40px" }}>Up</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+
+        </ScrollPage>
+        <ScrollPage page={2}>
+          <Animator animation={FadeUp}>
+            <span style={{ fontSize: "40px" }}>Down⛅️</span>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer >
+    </div >
   );
 }
 
